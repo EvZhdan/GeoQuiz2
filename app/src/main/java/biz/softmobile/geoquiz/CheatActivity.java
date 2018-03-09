e492686a361891122f7681b6dd94c347b71cea7b
EvZhdan <ev107@mail.ru>
2018-03-09 20:33:46 +0300

    3

--- app/src/main/java/biz/softmobile/geoquiz/CheatActivity.java
+++ app/src/main/java/biz/softmobile/geoquiz/CheatActivity.java
@@ -9,5 +9,6 @@ public class CheatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+        //Для коммита проба
     }
 }
